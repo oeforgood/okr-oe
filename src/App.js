ab00bc4a9400fd51d044d86eb0cc27e80ce8541a
François-Xavier Henry <fx@macbookair.home>
2026-09-09 20:05:08 +0200

    Fix: fragment JSX isMobile UpdatePage corrigé

--- src/App.js
+++ src/App.js
@@ -687,7 +687,6 @@ function MessagesPanel({managerNotifs,teammateNotifs=[],onReadNotif,onMarkAsRead
           <span style={{fontSize:15,fontWeight:600}}>{selected.title}</span>
           
         </div>
-      </>
         <div style={{fontSize:11,color:"#9e9890",marginBottom:4}}>{selected.date.toLocaleDateString("fr-FR",{weekday:"long",day:"numeric",month:"long"})} à {selected.date.toLocaleTimeString("fr-FR",{hour:"2-digit",minute:"2-digit"})}</div>
         {!selected.isSystem&&selected.notif?.weekKey&&<div style={{fontSize:11,color:"#9e9890",marginBottom:18}}>Semaine du {fmtWeekLabel(selected.notif.weekKey)}</div>}
         {selected.isSystem&&<div style={{marginBottom:14}}/>}
@@ -1994,6 +1993,7 @@ function UpdatePage({teamMember,questions,onSubmit,onDelete,onBack,onGoOKR,onGoU
 
       </div>
 
+      </>
 
       <div style={{fontSize:13,color:"#6b6560",marginBottom:16}}>Semaine du {weekLabel}</div>
 
